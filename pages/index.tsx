7519b4e7afdfef9c12b20d268422bae4c882c274
sorious77 <sorious77@naver.com>
2022-08-14 21:28:40 +0900

    #4.1 Test Drive part One

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,28 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">apply tailwind</h1>
+    <div className="bg-slate-200 py-6 px-5 flex flex-col space-y-5">
+      <div className="bg-white p-10 rounded-2xl shadow-md">
+        <span className="text-2xl font-semibold">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-bold">$170</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-bold">$800</span>
+        </div>
+        <div className="flex justify-between my-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-bold">$970</span>
+        </div>
+        <div className="mt-3 bg-blue-500 text-white rounded-xl p-5 w-1/2 text-center mx-auto text-lg">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-2xl shadow-md"></div>
+      <div className="bg-white p-6 rounded-2xl shadow-md"></div>
+      <div className="bg-white p-6 rounded-2xl shadow-md"></div>
     </div>
   );
 };
