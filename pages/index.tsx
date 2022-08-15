10cac990be1d8107ecc86214d73396fa22a0af3c
sorious77 <sorious77@naver.com>
2022-08-15 22:23:02 +0900

    #4.11 Dark Mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,26 +2,33 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-200 py-6 px-5 space-y-5 min-h-screen grid gap-10 lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center">
-      <div className="bg-white p-10 rounded-2xl shadow-md flex flex-col justify-between">
-        <span className="text-2xl font-semibold">Select Item</span>
+    <div className="bg-slate-200 py-6 px-5 space-y-5 min-h-screen grid gap-10 lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center dark">
+      <div className="bg-white dark:bg-black p-10 rounded-2xl shadow-md flex flex-col justify-between">
+        <span className="text-2xl font-semibold dark:text-white">
+          Select Item
+        </span>
         <ul>
           {[1, 2, 3, 4, 5].map((i) => (
             <div className="flex justify-between my-2" key={i}>
-              <span className="text-gray-500">Grey Chair</span>
-              <span className="font-bold">$170</span>
+              <span className="text-gray-500 dark:text-gray-100">
+                Grey Chair
+              </span>
+              <span className="font-bold dark:text-white">$170</span>
             </div>
           ))}
         </ul>
-        <div className="flex justify-between my-2 pt-2 border-t-2 border-dashed">
+        <div className="flex justify-between my-2 pt-2 border-t-2 border-dashed dark:text-white">
           <span>Total</span>
           <span className="font-bold">$970</span>
         </div>
-        <button className="mt-5 bg-blue-500 text-white rounded-xl p-3 w-3/4 text-center block mx-auto text-lg hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
+        <button
+          className="mt-5 bg-blue-500 dark:bg-black dark:border-white dark:border text-white
+          rounded-xl p-3 w-3/4 text-center block mx-auto text-lg hover:bg-teal-500 hover:text-black dark:hover:bg-white active:bg-yellow-500 focus:bg-red-500"
+        >
           Checkout
         </button>
       </div>
-      <div className="bg-white rounded-2xl shadow-md overflow-hidden group">
+      <div className="bg-white rounded-2xl shadow-md overflow-hidden group dark">
         <div className="portrait:bg-blue-500 landscape:bg-indigo-300 p-6 pb-14 xl:pb-40">
           <span className="text-white text-2xl">Profile</span>
         </div>
@@ -43,7 +50,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-6 rounded-2xl shadow-md lg:col-span-2 xl:col-span-1">
+      <div className="bg-white p-6 rounded-2xl shadow-md lg:col-span-2 xl:col-span-1 dark">
         <div className="flex justify-between mb-3">
           <span>⬅️</span>
           <div className="space-x-3">
