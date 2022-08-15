ce68d4eb59afbd2e65af778fba63a7264a20d684
sorious77 <sorious77@naver.com>
2022-08-15 20:52:47 +0900

    #4.8 More Modifiers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,12 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <form className="flex flex-col space-y-2 p-5">
-      <input
-        type="text"
-        required
-        placeholder="Username"
-        className="border p-1 peer border-gray-400 rounded"
-      />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+    <div className="flex flex-col space-y-2 p-5">
+      <details className="select-none open:bg-red-500">
+        <summary className="cursor-pointer">What is my favorite food.</summary>
+        <div>Chicken</div>
+      </details>
+    </div>
   );
 };
 
