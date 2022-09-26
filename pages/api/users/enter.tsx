26c5672e0e3d56be1aeb46f2e415d5cbe692a0f8
sorious77 <sorious77@naver.com>
2022-09-26 23:43:38 +0900

    #8.4 withHandler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,15 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../../libs/server/client";
+import withHandler from "../../../libs/server/windHandler";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
-  console.log(req.body);
-
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   res.json({ ok: true });
-  res.status(200).end();
+  console.log(req.body);
+  return res.status(200).end();
 }
+
+export default withHandler("POST", handler);
