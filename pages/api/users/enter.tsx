cb2fcac303f918afa3dfbeb5a5228b567bb83840
sorious77 <sorious77@naver.com>
2022-09-28 00:22:48 +0900

    #9.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,69 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/windHandler";
+import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.json({ ok: true });
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+
+  /*
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      console.log("Found User.", user);
+    }
+
+    if (!user) {
+      console.log("Cannot find User. Create New User");
+
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+  } else if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+
+    if (user) {
+      console.log("Found User.", user);
+    }
+
+    if (!user) {
+      console.log("Cannot find User. Create New User");
+
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+  }
+  */
+
+  console.log(payload);
   return res.status(200).end();
 }
 
