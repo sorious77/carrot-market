b27038f9e49cc7ee5607df47e59111d404ae0e05
sorious77 <sorious77@naver.com>
2022-09-28 21:31:11 +0900

    #9.3 Twilo Setup

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/windHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+
+  if (!user) {
+    return res.status(400).json({ ok: false });
+  }
+
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
   const token = await client.token.create({
@@ -24,54 +32,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     },
   });
 
-  /*
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (user) {
-      console.log("Found User.", user);
-    }
-
-    if (!user) {
-      console.log("Cannot find User. Create New User");
-
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-  } else if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-
-    if (user) {
-      console.log("Found User.", user);
-    }
-
-    if (!user) {
-      console.log("Cannot find User. Create New User");
-
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-  }
-  */
-
   console.log(token);
-  return res.status(200).end();
+  return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
